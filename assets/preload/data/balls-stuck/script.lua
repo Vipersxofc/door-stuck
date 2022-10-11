8887a07fa3927a8db0afaa490ebcb31ab1432144
Vipersxofc <114888032+Vipersxofc@users.noreply.github.com>
2022-10-11 14:31:11 -0300

    Add files via upload

---
+++ assets/preload/data/balls-stuck/script.lua
@@ -0,0 +1,66 @@
+function onCreate()
+    setProperty('gf.visible', false)
+    setProperty('explode.alpha',0)
+
+	makeLuaSprite('card', 'nothing', -400, 250);
+	setObjectCamera('card', 'hud');
+	addLuaSprite('card');
+	scaleObject('card', 0.7, 0.7);
+
+	makeLuaText('songname', 'Song: Balls Stuck', '500', -462, 262);
+	setTextSize('songname', 24);
+	setObjectCamera('songname', 'hud');
+	setTextColor('songname', 'A4A4A4');
+	setTextFont('songname', 'stratum2-medium-webfont.ttf');
+	setTextBorder('songname', '0','A4A4A4');
+	addLuaText('songname');
+
+	makeLuaText('desc', 'Song by: Tsuraran', '500', -517, 320);
+	setTextSize('desc', 24);
+	setObjectCamera('desc', 'hud');
+	setTextColor('desc', 'A4A4A4');
+	setTextFont('desc', 'stratum2-medium-webfont.ttf');
+	setTextBorder('desc', '0','A4A4A4');
+	addLuaText('desc');
+	
+	makeLuaSprite('bartop','',0, -30);
+	makeGraphic('bartop',1280,100,'000000');
+	addLuaSprite('bartop', false);
+	setObjectCamera('bartop','hud');
+	setScrollFactor('bartop',0,0);
+
+	makeLuaSprite('barbot','',0,650);
+	makeGraphic('barbot',1280,100,'000000');
+	addLuaSprite('barbot', false);
+	setScrollFactor('barbot',0,0);
+	setObjectCamera('barbot','hud');
+end
+
+function onCountdownTick(counter)
+	if counter == 0 then
+		doTweenX('card','card', '0', 1, 'expoInOut')
+		doTweenX('name','songname', '-92', 1, 'expoInOut')
+		doTweenX('line1','desc', '-130', 1, 'expoInOut')
+		runTimer('cardback', 4)
+	end
+end
+
+function onTweenCompleted(tag)
+	if tag == 'dawg' then
+		removeLuaSprite('dawg', true);
+		removeLuaText('desc', true);
+		removeLuaText('songname', true);
+	end
+end
+
+function onTimerCompleted(tag, loops, loopsLeft)
+	if tag == 'aaargh' then		
+		triggerEvent('Screen Shake', '0.7, 0.006', '0.7, 0.006')
+	end
+
+	if tag == 'cardback' then
+		doTweenX('dawg','card', '-400', 1, 'expoInOut')
+		doTweenX('andnamowashisnamo','songname', '-462', 1, 'expoInOut')
+		doTweenX('lineo1','desc', '-517', 1, 'expoInOut')
+	end
+end
